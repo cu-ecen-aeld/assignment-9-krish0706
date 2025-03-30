478cd315d9f21d4cf4e76fae94c817f1fe2bf559
kshah <krishsowmilshah@gmail.com>
2025-03-30 03:52:13 -0600

    Updates commit ref

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-AESDCHAR_VERSION = 'c5de76e2771f8cc2ea1f337f83fbe26936d9a5f5'
+AESDCHAR_VERSION = 'f4b2a322284121769d7af4a9778435e76d525479'
 AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-krish0706.git'
 AESDCHAR_SITE_METHOD = git
 AESDCHAR_GIT_SUBMODULES = YES
